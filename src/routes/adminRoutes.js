3a0b2e21063ef4b2ac131614f8afc0d36f5dd8eb
gneris177 <gneris177@gmail.com>
2025-04-01 08:53:31 -0300

    update

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,10 +1,15 @@
 import { Router } from 'express';
 import { GiftsController } from '../controllers/admin/GiftsController.js';
+import ManageUsersController from '../controllers/admin/ManageUsersController.js';
 
 const router = Router();
 
 const giftsController = new GiftsController(); 
+const manageUsersController = new ManageUsersController(); 
 
 router.get('/gifts', giftsController.getGifts);
+router.get('/users', manageUsersController.listUsers);
+router.get('/users/:user_id', manageUsersController.getUser);
+router.get('/user-events/:user_id', manageUsersController.getUserEvents);
 
 export default router;
