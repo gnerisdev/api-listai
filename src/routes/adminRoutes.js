ef324fb3da8d7fd5fbcc1108443e618d94862971
Bruno_Henrique <82627367+Bruno7788@users.noreply.github.com>
2025-04-02 00:29:06 -0300

    Update adminRoutes.js

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
-import { GiftsController } from '../controllers/admin/GiftsController.js';
+import { GiftsController } from '../../controllers/admin/GiftsController.js';
 
 const router = Router();
 
 const giftsController = new GiftsController(); 
 
 router.get('/gifts', giftsController.getGifts);
+router.post('/register-gift', giftsController.registerGift);
+router.put('/update-gifts/:id', giftsController.updateGift);
+router.delete('/delete-gifts/:id', (req, res) => giftsController.removeGift(req, res))
 
 export default router;
