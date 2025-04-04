12cce1d5390c8e7303654b57360798fe5de5cbe5
Brunuz7 <Brunuz.dev@gmail.com>
2025-04-04 20:27:56 -0300

    criação AuthController

--- src/routes/admin/main.js
+++ src/routes/admin/main.js
@@ -5,6 +5,9 @@ const router = Router();
 
 const giftsController = new GiftsController(); 
 
-router.get('/admin/gifts', giftsController.getGifts);
+router.get('/gifts', giftsController.getGifts);
+router.post('/register-gift', giftsController.registerGift);
+router.put('/update-gifts/:id', giftsController.updateGift);
+router.delete('/delete-gifts/:id', (req, res) => giftsController.removeGift(req, res))
 
 export default router;
