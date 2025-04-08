119e131b877d8a534f31f12cc90422fe3790540e
gneris177 <gneris177@gmail.com>
2025-04-07 23:56:10 -0300

    feat: new routes

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import { GiftsController } from '../controllers/admin/GiftsController.js';
 import ManageUsersController from '../controllers/admin/ManageUsersController.js';
+import EventsController from '../controllers/admin/EventsController.js';
 
 const router = Router();
 
 const giftsController = new GiftsController(); 
 const manageUsersController = new ManageUsersController(); 
+const eventsController = new EventsController(); 
 
 // Gifts
 router.get('/gifts', giftsController.getGifts);
@@ -16,5 +18,8 @@ router.delete('/gifts/:id', (req, res) => giftsController.removeGift(req, res))
 router.get('/users', manageUsersController.listUsers);
 router.get('/users/:user_id', manageUsersController.getUser);
 router.get('/user-events/:user_id', manageUsersController.getUserEvents);
+// Events
+router.get('/events', eventsController.getEvents);
+router.get('/event-details/:event_id', eventsController.getDetails);
 
 export default router;
