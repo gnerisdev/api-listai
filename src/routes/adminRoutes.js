deae01080a4b36fe537db0bd32d170dbbd50e165
Bruno_Henrique <82627367+Bruno7788@users.noreply.github.com>
2025-04-07 22:57:02 -0300

    Add router Login - adminRoutes.js
    Adicinando as rotas de login ao adminRoutes.js

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { GiftsController } from '../controllers/admin/GiftsController.js';
 import ManageUsersController from '../controllers/admin/ManageUsersController.js';
+import AuthController from '../controllers/admin/AuthController.js';
 
 const router = Router();
 
@@ -12,6 +13,8 @@ router.get('/gifts', giftsController.getGifts);
 router.post('/gifts', giftsController.registerGift);
 router.put('/gifts/:id', giftsController.updateGift);
 router.delete('/gifts/:id', (req, res) => giftsController.removeGift(req, res))
+//Login
+router.post('/login', AuthController.login);
 // Users
 router.get('/users', manageUsersController.listUsers);
 router.get('/users/:user_id', manageUsersController.getUser);
