084b95c6ef595b5d2820e755aa499f11f1c77d54
Brunuz7 <Brunuz.dev@gmail.com>
2025-05-07 21:49:27 -0300

    task painel de configuração

--- src/controllers/users/EventSettingsController.js
+++ src/controllers/users/EventSettingsController.js
@@ -7,25 +7,76 @@ class EventSettingsController {
   async getSettings(req, res) {
     try {
       const eventId = parseInt(req.params.event_id);
-      const userId = parseInt(req.headers.user_id);
+      const userId = parseInt(req.headers['user_id']);
 
-      // Verify user permission event
-      const event = await prisma.users_events.findFirst({
-        where: { user_id: userId, event_id: eventId },
-        include: { event: true },
+      if (!userId || isNaN(userId) || !eventId || isNaN(eventId)) {
+        return res.status(400).json({ 
+          success: false, 
+          message: 'IDs de usuário ou evento inválidos.' 
+        });
+      }
+      const userEvent = await prisma.users_events.findFirst({
+        where: { 
+          user_id: userId, 
+          event_id: eventId 
+        }
       });
 
-      if (!event) {
-        return res.status(404).json({ success: false, message: 'Evento não encontrado.' });
+      if (!userEvent) {
+        return res.status(403).json({ 
+          success: false, 
+          message: 'Acesso não autorizado.' 
+        });
       }
 
       const eventSettings = await prisma.event_settings.findFirst({
-        where: { event_id: eventId }, 
-        include: { event: true }, 
+        where: {
+          user_id: userId,
+          event_id: eventId,
+        },
+        include: {
+          event: true
+        },
+      });
+      
+      if (!eventSettings) {
+        eventSettings = await prisma.event_settings.create({
+          data: {
+            user_id: userId,
+            event_id: eventId,
+            show_gift_list: true,
+            show_guest_messages: true,
+            show_event_info: true,
+            allow_guest_confirmation: true
+          },
+          include: {
+            event: true
+          }
+        });
+      }
+      // Verify user permission event
+      const event = await prisma.users_events.findFirst({
+        where: { 
+          user_id: userId, 
+          event_id: eventId 
+        },
+        include: { 
+          event: true 
+        },
       });
 
+      if (!event) {
+        return res.status(404).json({ 
+          success: false, 
+          message: 'Evento não encontrado.' 
+        });
+      }
+
       if (!eventSettings) {
-        return res.status(404).json({ success: false, message: 'Configurações do evento não encontradas.' });
+        return res.status(404).json({ 
+          success: false, 
+          message: 'Configurações do evento não encontradas.' 
+        });
       }
 
       return res.status(200).json({
@@ -38,22 +89,33 @@ class EventSettingsController {
           show_guest_messages: eventSettings.show_guest_messages,
           show_event_info: eventSettings.show_event_info,
           allow_guest_confirmation: eventSettings.allow_guest_confirmation,
-        }
+        },
+  
       });
     } catch (error) {
       console.log(error);
       LogUtils.errorLogger(error);
-      return res.status(500).json({ success: false, message: 'Erro ao buscar configurações do evento.' });
+      return res.status(500).json({ 
+        success: false, 
+        message: 'Erro ao buscar configurações do evento.' 
+      });
     }
   }
 
   async update(req, res) {
     try {
       const eventId = parseInt(req.params.event_id);
-      const { show_gift_list, show_guest_messages, show_event_info, allow_guest_confirmation } = req.body;
+      const { 
+        show_gift_list, 
+        show_guest_messages, 
+        show_event_info, 
+        allow_guest_confirmation } = req.body;
 
       if (!eventId) {
-        return res.status(400).json({ success: false, message: 'Evento não fornecido.' });
+        return res.status(400).json({ 
+          success: false, 
+          message: 'Evento não fornecido.' 
+        });
       }
 
       if (typeof show_gift_list !== 'boolean') {
